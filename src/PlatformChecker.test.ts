fd51a064574ab662f770c389f40a9e25bc7208ed
TaraTritt <taramiwatritt@gmail.com>
2019-03-20 14:05:43 -0400

    Removed unused async in PlatformChecker tests

--- src/PlatformChecker.test.ts
+++ src/PlatformChecker.test.ts
@@ -18,31 +18,31 @@ describe('PlatformChecker', () => {
       window.navigator.userAgent = ''
     })
 
-    it('returns true for iPhone', async () => {
+    it('returns true for iPhone', () => {
       window.navigator.userAgent = 'iPhone'
       const platformChecker = new PlatformChecker()
       expect(platformChecker.isSupportedPlatform()).toEqual(true)
     })
 
-    it('returns true for iPad', async () => {
+    it('returns true for iPad', () => {
       window.navigator.userAgent = 'iPad'
       const platformChecker = new PlatformChecker()
       expect(platformChecker.isSupportedPlatform()).toEqual(true)
     })
 
-    it('returns true for react native', async () => {
+    it('returns true for react native', () => {
       window.navigator.userAgent = 'reactnative'
       const platformChecker = new PlatformChecker()
       expect(platformChecker.isSupportedPlatform()).toEqual(true)
     })
 
-    it('returns false for android', async () => {
+    it('returns false for android', () => {
       window.navigator.userAgent = 'Android'
       const platformChecker = new PlatformChecker()
       expect(platformChecker.isSupportedPlatform()).toEqual(false)
     })
 
-    it('returns false for embedded ios browser', async () => {
+    it('returns false for embedded ios browser', () => {
       (window as any).webkit = {
         messageHandlers: {}
       }
